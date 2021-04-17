c9d1519245d1060a6fa3018a6edb48ce2c7049e1
voidless <juliusjedermann@gmail.com>
2021-04-18 01:57:08 +0200

    put places api into one file

--- osmcheck/api/here_places_check.py
+++ osmcheck/api/here_places_check.py
@@ -1,8 +1,33 @@
-from here_places_api import here_api_request
-from osmcheck.api import OsmEntry, register_check, Check, CheckTimestamp
+import json
+
+import requests
+
+from osmcheck.api import OsmEntry, register_check, Check
 
 
-#@register_check
+def here_api_request(name, latitude, longitude) -> dict:
+    with open("../../here_places_api_key", "r") as keyfile:
+        api_token = keyfile.read()
+        keyfile.close()
+
+    request_params = {
+            'in': 'circle:{},{};r=100'.format(latitude, longitude),
+            'q': name,
+            'apiKey' : '{}'.format(api_token)
+        }
+
+    r = requests.get(
+        'https://discover.search.hereapi.com/v1/discover',
+        params = request_params
+    )
+    if(r.status_code == 401):
+        raise Exception("Bad Here Places api Key")
+    r.raise_for_status()
+
+    result = json.loads(r.text)
+    return result
+
+@register_check
 class HerePlacesCheck(Check):
 
     @staticmethod
