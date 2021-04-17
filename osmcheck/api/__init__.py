43235e18e96bc647d1ab31d69cef7c23c032051b
Alvar Penning <post@0x21.biz>
2021-04-17 17:36:59 +0200

    CheckWebsite: check an entry by its website

--- osmcheck/api/__init__.py
+++ osmcheck/api/__init__.py
@@ -1,10 +1,12 @@
 __all__ = [
-    "osm",
     "check",
     "check_timestamp",
+    "check_website",
+    "osm",
 ]
 
 from .osm import *
 
 from .check import check
 from .check_timestamp import *
+from .check_website import *
---
+++ osmcheck/api/check_website.py
@@ -0,0 +1,23 @@
+import requests
+
+from .check import Check, register_check
+from .osm import OsmEntry
+
+
+@register_check
+class CheckWebsite(Check):
+    """ Some OsmEntries have a website within their tags. This Check tries to
+        access this webpage. A non-existing webpage might be a hint.
+    """
+
+    @staticmethod
+    def eval(entry: OsmEntry) -> float:
+        if not "website" in entry.tags:
+            return 0.5
+
+        try:
+            r = requests.get(entry.tags["website"])
+            r.raise_for_status()
+            return 0.75
+        except:
+            return 0.25
