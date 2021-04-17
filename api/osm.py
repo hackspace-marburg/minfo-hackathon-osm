23ade749cdaaaabc87130374c0936818572807d7
Alvar Penning <post@0x21.biz>
2021-04-17 15:03:22 +0200

    api/osm.py: include timestamp of last modification

--- api/osm.py
+++ api/osm.py
@@ -1,28 +1,41 @@
 import overpass
 
-from typing import Dict, List, NamedTuple, Tuple
+from datetime import datetime
+from typing import Dict, List, NamedTuple
 
 
 class OsmEntry(NamedTuple):
-    "Representation of shops resp. point entries in the OSM."
+    "Representation of a shops entry in OSM."
     osm_id: int
-    location: Tuple[float]
-    properties: Dict[str, str]
+    lat: float
+    lon: float
+    timestamp: datetime
+    tags: Dict[str, str]
 
     def __repr__(self) -> str:
-        if "name" in self.properties:
-            return f"OsmEntry({self.osm_id}, {self.properties['name']})"
+        if "name" in self.tags:
+            return f"OsmEntry({self.osm_id}, {self.tags['name']})"
         return f"OsmEntry({self.osm_id})"
 
 
 def query_osm(south: float, west: float, north: float, east: float) -> List[OsmEntry]:
     "List all shops within the specified bounding box."
     api = overpass.API()
-    resp = api.get(f"node['shop']({south},{west},{north},{east});")
+    resp = api.get(
+        f"node['shop']({south},{west},{north},{east});",
+        verbosity="meta",
+        responseformat="json",
+    )
     return [
-        OsmEntry(f.id, f.geometry.coordinates, f.properties)
-        for f in resp.features
-        if f.geometry.type == "Point"
+        OsmEntry(
+            ele["id"],
+            ele["lat"],
+            ele["lon"],
+            datetime.strptime(ele["timestamp"], "%Y-%m-%dT%H:%M:%SZ"),
+            ele["tags"],
+        )
+        for ele in resp["elements"]
+        if ele["type"] == "node"
     ]
 
 
