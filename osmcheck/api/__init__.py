6a0a7b2c74c93c04082c0abecbdb6d27e527b908
psychophoniac <sf@psychophoniac.de>
2021-04-18 00:31:07 +0200

    lieferando check

--- osmcheck/api/__init__.py
+++ osmcheck/api/__init__.py
@@ -4,6 +4,7 @@ __all__ = [
     "check_osm_closed",
     "check_timestamp",
     "check_website",
+    "check_lieferando",
     "osm",
 ]
 
@@ -14,3 +15,4 @@ from .check_gelbe_seiten import *
 from .check_osm_closed import *
 from .check_timestamp import *
 from .check_website import *
+from .check_lieferando import *
