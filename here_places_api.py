40bc9ef27d4abd1e84044b716d456f8f6fa8ba0a
voidless <juliusjedermann@gmail.com>
2021-04-17 16:06:52 +0200

    added exceptions

--- here_places_api.py
+++ here_places_api.py
@@ -1,16 +1,30 @@
 import requests
 import sys
+import json
+from pprint import pprint
 
 api_token = sys.argv[1]
 
-print("Here Places api token: {}".format(api_token))
-r = requests.get(
-    'https://discover.search.hereapi.com/v1/discover',
-    params = {
-        'at' : '52.5228,13.4124',
-        'q' : 'petrol+station',
-        'apiKey' : '{}'.format(api_token)
-    })
 
-print(r.headers)
-print(r.content)
+def here_api_request(name, latitude, longitude) -> dict:
+    print("Here Places api token: {}".format(api_token))
+    request_params = {
+            'at' : '{},{}'.format(latitude, longitude),
+            'q' : name,
+            'apiKey' : '{}'.format(api_token)
+        }
+    r = requests.get(
+        'https://discover.search.hereapi.com/v1/discover',
+        params = request_params
+    )
+
+    if(r.status_code == 401):
+        raise Exception("Bad Here Places Api Key")
+    r.raise_for_status()
+    print(r.headers)
+    print(r.status_code)
+    print(r.text)
+    result = json.loads(r.text)
+    pprint(result)
+
+here_api_request('döner', 50.8161222, 8.7784447)
