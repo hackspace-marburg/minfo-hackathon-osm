53cac03b9ededceb40cc662efb9895b43ffb07f7
Alvar Penning <post@0x21.biz>
2021-04-18 10:36:24 +0200

    Integrate here places

--- osmcheck/api/check_here_places.py
+++ osmcheck/api/check_here_places.py
@@ -1,27 +1,29 @@
 import json
 import os
-
 import requests
 
-from osmcheck.api import OsmEntry, register_check, Check
+from .check import Check, register_check
+from .osm import OsmEntry
 
 
 def here_api_request(name, latitude, longitude) -> dict:
+    if not os.path.isfile("here_places_api_key"):
+        print("Error: API key is missing")
+        return {}
+
     with open("here_places_api_key", "r") as keyfile:
         api_token = keyfile.read()
-        keyfile.close()
 
     request_params = {
-        'in': 'circle:{},{};r=100'.format(latitude, longitude),
-        'q': name,
-        'apiKey': '{}'.format(api_token)
+        "in": f"circle:{latitude},{longitude};r=100",
+        "q": name,
+        "apiKey": api_token,
     }
 
     r = requests.get(
-        'https://discover.search.hereapi.com/v1/discover',
-        params=request_params
+        "https://discover.search.hereapi.com/v1/discover", params=request_params
     )
-    if (r.status_code == 401):
+    if r.status_code == 401:
         raise Exception("Bad Here Places api Key")
     r.raise_for_status()
 
@@ -30,29 +32,24 @@ def here_api_request(name, latitude, longitude) -> dict:
 
 
 @register_check
-class HerePlacesCheck(Check):
-
+class CheckHerePlaces(Check):
     @staticmethod
     def eval(entry: OsmEntry):
         # if no name tag exists, this check cant be used
-        if not 'name' in entry.tags:
+        if not "name" in entry.tags:
             return None
 
-        items = here_api_request(entry.tags['name'], entry.lat, entry.lon)
-        if not 'items' in items:
+        items = here_api_request(entry.tags["name"], entry.lat, entry.lon)
+        if not "items" in items:
             print("Error: Answer did not contain a key called 'items'")
             return None
 
-        return (.75 if (len(items['items']) > 0) else .2)
+        return 0.75 if (len(items["items"]) > 0) else 0.2
 
 
 if __name__ == "__main__":
     testEntry = OsmEntry(
-        400,
-        '50.8097633',
-        '8.7722088',
-        'test',
-        {'name': 'Shawarma Haus'},
+        400, "50.8097633", "8.7722088", "test", {"name": "Shawarma Haus"},
     )
 
     print(HerePlacesCheck.eval(testEntry))
